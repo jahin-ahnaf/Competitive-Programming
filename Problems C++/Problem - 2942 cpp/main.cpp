725fe79c49ae124b53ecf5cf85f5e8c619f102bb
Jahin Ahnaf <jahinahnaf16@gmail.com>
2024-10-30 22:48:48 +0600

    GG

--- Problems C++/Problem - 2942 cpp/main.cpp
+++ Problems C++/Problem - 2942 cpp/main.cpp
@@ -1,6 +1,28 @@
-#include <bits\stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-int main(){
-    
+int main()
+{
+    int count = 0;
+    int received = 0;
+    int n, c;
+    cin >> n >> c;
+    int a[n];
+    int len = sizeof(a) / sizeof(a[0]);
+
+    for (int i = 0; i < len; i++)
+    {
+        cin >> a[i];
+        if (i == 0)
+        {
+            count++;
+            received = a[0];
+        }
+        else if (a[i] - received >= c)
+        {
+            count++;
+            received = a[i];
+        }
+    }
+    cout << count;
 }
