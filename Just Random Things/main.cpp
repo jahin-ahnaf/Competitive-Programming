ac995d9267863ca4333fc1a00d12d523c2d8a4f2
Jahin Ahnaf <jahinahnaf16@gmail.com>
2024-10-31 20:19:48 +0600

    GG

--- Just Random Things/main.cpp
+++ Just Random Things/main.cpp
@@ -3,26 +3,50 @@ using namespace std;
 
 int main()
 {
-    int count = 0;
-    int received = 0;
-    int n, c;
-    cin >> n >> c;
-    int a[n];
-    int len = sizeof(a) / sizeof(a[0]);
-
-    for (int i = 0; i < len; i++)
+    int t;
+    cin >> t;
+    for (int i = 0; i < t; i++)
     {
-        cin >> a[i];
-        if (i == 0)
+        string p;
+        cin >> p;
+        string pos[2];
+        pos[0] = p[0]; // row d
+        pos[1] = p[1]; // column 5
+
+        char row[8] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'};
+        int column[8] = {1, 2, 3, 4, 5, 6, 7, 8};
+
+        vector<char> new_row;
+        vector<int> new_column;
+
+        for (int j = 0; j < 8; j++)
         {
-            count++;
-            received = a[0];
+            if (row[j] == pos[0][0])
+            {
+                continue;
+            }
+            else
+            {
+                new_row.push_back(row[j]);
+            }
         }
-        else if (a[i] - received >= c)
+        for (int j = 0; j < 8; j++)
         {
-            count++;
-            received = a[i];
+            if (column[j] == pos[1][0] - '0')
+            {
+                continue;
+            }
+            else
+            {
+                new_column.push_back(column[j]);
+            }
+        }
+        // total by row
+        for (int j = 0; j < 7; j++){
+            cout << pos[0] << new_column[j] << endl;
+        }
+        for (int j = 0; j < 7; j++){
+            cout << new_row[j] << pos[1] << endl;
         }
     }
-    cout << count;
 }
