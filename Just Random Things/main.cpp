87e942367151c2c0bf8aaf80b49cdedc57e7fd1c
Jahin Ahnaf <jahinahnaf16@gmail.com>
2024-11-02 06:15:41 +0600

    bruh

--- Just Random Things/main.cpp
+++ Just Random Things/main.cpp
@@ -3,18 +3,5 @@ using namespace std;
 
 int main()
 {
-    int k, r;
-    cin >> k >> r;
-
-    int ans = 1;
-
-    while ((k*ans)%10 != r){
-        if ((k*ans)%10 == 0){
-            break;
-        }
-        else{
-            ans++;
-        }
-    }
-    cout << ans;
+    
 }
