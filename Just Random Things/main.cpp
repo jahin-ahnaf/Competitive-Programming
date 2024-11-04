c783c499bd286f1552a3033b679045bce7313990
Jahin Ahnaf <jahinahnaf16@gmail.com>
2024-11-04 08:10:50 +0600

    -_-

--- Just Random Things/main.cpp
+++ Just Random Things/main.cpp
@@ -1,31 +1,10 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main()
-{
-    int t, a;
-    int ans = 0;
-    cin >> t;
-    int b[t];
-    int count = -1;
+void solve(){
+    cout << "BRUH";
+}
 
-    int i = 0;
-    while (i < t)
-    {
-        cin >> a;
-
-        for (int i = 0; i < a; i++)
-        {
-            cin >> b[i];
-        }
-        sort ((b), b + a);
-        for (int i = 0; i < a; i++){
-            if (b[i] == b[i + 1] && b[i + 1] == b[i + 2] && b[i + 2] == b[i]){
-                ans = b[i];
-                continue;
-            }
-        }
-        cout << ans << endl;
-        i++;
-    }
+int main() {
+    solve();
 }
