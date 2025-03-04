01d78516aa7b2434255cb688357f3efe13b411bc
JA Tech <jahinahnaf16@gmail.com>
2025-03-04 23:00:44 +0600

    yahoo

--- running codeforces/main.cpp
+++ running codeforces/main.cpp
@@ -1,14 +1,16 @@
 #include <bits/stdc++.h>
+#define ll long long int
 using namespace std;
 
+//By the name of Allah
 void solve(){
     
 }
 
 int main()
 {
-    int t;
-    cin >> t;
+    int t = 1;
+    //cin >> t;
     while (t--){
         solve();
     }
